caa5653198296c76b6f803afaa6ae96a0fd96b45
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-01 18:46:23 +0200

    new component todo item

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,27 +1,21 @@
-export function TodoList({ todos }){
+import { TodoItem } from "./TodoItem"
+
+export function TodoList({ todos, toggleTodo, deleteTodo }){
     return (
 
         <ul className="list">
             {todos.length === 0 && "No Todos"}
                 {todos.map(todo => {
                     return (
-                        <li key={todo.id}>
-                            <label>
-                            <input 
-                                type="checkbox" 
-                                checked={todo.completed} 
-                                // onChange={(e) => toggleTodo(todo.id, e.target.checked)}
-                                
-                            />
-                            {todo.title}
-                            </label>
-                            <button 
-                                className="btn btn-danger"
-                                // onClick={() => deleteTodo(todo.id)}
-                            >
-                            Delete
-                            </button>
-                        </li>
+                        <TodoItem 
+                        {...todo}
+                            // id={todo.id} 
+                            // completed={todo.completed} 
+                            // title={todo.title} 
+                            key={todo.id} 
+                            toggleTodo={toggleTodo}
+                            deleteTodo={deleteTodo}
+                        />
                     ) 
                 })}
         </ul>
